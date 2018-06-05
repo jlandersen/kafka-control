65226ffb04244fd950b15953005dc062614f445b
Jeppe Andersen <jeppe@outlook.com>
2018-06-05 16:02:28 +0200

    Add dynamic load of webpack in server
    Eliminates dependency on webpack when running in production mode

--- server/index.ts
+++ server/index.ts
@@ -1,62 +1,68 @@
 import express from "express";
 import path from "path";
 import socketio from "socket.io";
-import webpack from "webpack";
-import webpackMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
 
 import clientManager from "./ClientManager";
-
-const webpackDevConfig = require("../../webpack.config.js");
 
 const app = express();
 const router = express.Router();
 const isDevelopment = process.env.NODE_ENV !== "production";
 const port = isDevelopment ? 3000 : Number(process.env.PORT);
 
-if (isDevelopment) {
-    const compiler = webpack({ ...webpackDevConfig, mode: "development" });
-    const middleware = webpackMiddleware(compiler, {
-        publicPath: webpackDevConfig.output.publicPath,
-    });
+initServer(isDevelopment, port)
+    .then(console.log)
+    .catch(console.error);
 
-    app.use(middleware);
-    app.use(webpackHotMiddleware(compiler, {
-        log: false,
-        path: "/__webpack_hmr",
-        heartbeat: 10 * 1000,
-    }));
-}
+async function initServer(developmentMode: boolean, portNumber: number) {
+    if (isDevelopment) {
+        const webpackDevConfig = require("../../webpack.config.js");
+        const webpack = await import("webpack");
+        const webpackMiddleware = await import("webpack-dev-middleware");
+        const webpackHotMiddleware = await import("webpack-hot-middleware");
+        const compiler = webpack.default({ ...webpackDevConfig, mode: "development" });
+        const middleware = webpackMiddleware.default(compiler, {
+            publicPath: webpackDevConfig.output.publicPath,
+        });
 
-app.use(express.static(path.resolve(__dirname, "../")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../", "index.html"));
-});
-
-const server = app.listen(port, "0.0.0.0", (err: () => void | undefined) => {
-    if (err) {
-        console.log(err);
+        app.use(middleware);
+        app.use(webpackHotMiddleware.default(compiler, {
+            log: false,
+            path: "/__webpack_hmr",
+            heartbeat: 10 * 1000,
+        }));
     }
 
-    console.info("==> Listening on port %s.", port, port);
-});
+    app.use(express.static(path.resolve(__dirname, "../")));
 
-const io = socketio(server);
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../", "index.html"));
+    });
 
-io.on("connection", (socket) => {
-    clientManager.register(socket.id, (type, data) => {
-        socket.emit("action", {
-            type,
-            ...data,
+    const server = app.listen(portNumber, "0.0.0.0", (err: () => void | undefined) => {
+        if (err) {
+            console.log(err);
+        }
+
+        console.info("==> Listening on port %s.", portNumber, portNumber);
+    });
+
+    const io = socketio(server);
+
+    io.on("connection", (socket) => {
+        clientManager.register(socket.id, (type, data) => {
+            socket.emit("action", {
+                type,
+                ...data,
+            });
+        });
+
+        socket.on("disconnect", () => {
+            clientManager.deregister(socket.id);
+        });
+
+        socket.on("server/workspace/START", (data) => {
+            clientManager.connect(socket.id, data.brokers);
         });
     });
 
-    socket.on("disconnect", () => {
-        clientManager.deregister(socket.id);
-    });
-
-    socket.on("server/workspace/START", (data) => {
-        clientManager.connect(socket.id, data.brokers);
-    });
-});
+}
